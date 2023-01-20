8d853918d5df0de8b14d8bd54b5004cb53c38b82
Filipe Wolf Boeck <filipewolfboeck@MacBookAir.local>
2023-01-20 19:26:32 -0300

    cards de entrada, saida e total prontos

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -22,10 +22,24 @@ export function Dashboard() {
       </Header>
 
       <HighlightCards>
-        <HighlightCard />
-        <HighlightCard />
-        <HighlightCard />
-        <HighlightCard />
+        <HighlightCard 
+        type='up'
+        title='Entradas' 
+        amount='R$ 17.450,00' 
+        lastTransaction='Ultima dia 12 de setembro.' 
+        />
+        <HighlightCard 
+        type='down'
+        title='Saidas' 
+        amount='R$ 7.450,00' 
+        lastTransaction='Ultima dia 13 de setembro.' 
+        />
+        <HighlightCard 
+        type='total'
+        title='Total' 
+        amount='R$ 10.000,00' 
+        lastTransaction='Ultima dia 13 de setembro.' 
+        />
       </HighlightCards>
     </Container>
   )
