685a2932222f01ba514ffd9289fa08c1a8dab67a
Filipe Wolf Boeck <filipewolfboeck@MacBookAir.local>
2023-01-20 18:38:25 -0300

    trabalhada nas telas de cards, estilos diversos e insercao de libs

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import { HighlightCard } from '../../components/HighlightCard';
 
-import { Container, Title } from './styles';
+import {
+  Container, Header, UserInfo, Photo, User, UserGreeting, UserName, UserWrapper, Icon, HighlightCards
+} from './styles';
 
 export function Dashboard() {
-    return (
-        <Container>
-            <Title>
-                Dashboard
-            </Title>
-        </Container>
-    )
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/85641580?v=4' }} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Filipe</UserName>
+            </User>
+          </UserInfo>
+          <Icon name='power' />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCards>
+        <HighlightCard />
+        <HighlightCard />
+        <HighlightCard />
+        <HighlightCard />
+      </HighlightCards>
+    </Container>
+  )
 }
